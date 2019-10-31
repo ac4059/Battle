16974432066569ac708561dd2ee6e9546b9b2ef7
Arjun Roy <arjunroychoudhry4059@gmail.com>
2019-10-31 15:27:15 +0000

    Battle Entering Players

--- app.rb
+++ app.rb
@@ -2,7 +2,13 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
   get "/" do
-    "Testing infrastructure working!"
+    erb :index
+  end
+
+  post "/names" do
+    @playeronename = params[:playeronename]
+    @playertwoname = params[:playertwoname]
+    erb :play
   end
 
   run! if app_file == $0
--- spec/features/test_infrastructure_spec.rb
+++ spec/features/test_infrastructure_spec.rb
@@ -1,6 +1,9 @@
 feature "Battle" do
-  scenario "Testing infrastructure working!" do
-    visit "/"
-    expect(page).to have_text("Testing infrastructure working!")
-  end
+   scenario "Players fill in their name" do
+     visit "/"
+     fill_in "playeronename", :with => "Arjun"
+     fill_in "playertwoname", :with => "Nat"
+     click_button "Submit"
+     expect(page).to have_text("Arjun vs. Nat")
+   end
 end
